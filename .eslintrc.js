6e55c491b336943754a84b2f1489bee7945e7b17
dylan <1611185386@qq.com>
2024-01-21 22:15:57 +0800

    feat: 添加eslint的异步代码规范设置

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,35 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-essential',
-    'plugin:prettier/recommended'
+    'eslint-config-async',
+    'plugin:prettier/recommended',
   ],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'vue'],
-  rules: {}
+  plugins: ['@typescript-eslint', 'vue', 'simple-import-sort', 'eslint-plugin-node'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 0,
+    'simple-import-sort/imports': 'error',
+    'node/handle-callback-err': ['error', '^(e|err|error)$'],
+  },
 };
